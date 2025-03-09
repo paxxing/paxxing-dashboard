4758abb681a7de7f326e090d5290ff87c06035fd
tyange <usun16@gmail.com>
2025-03-09 22:31:01 +0900

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LucideAngularModule, Menu, Ellipsis } from 'lucide-angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LucideAngularModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  readonly MenuIcon = Menu;
+  readonly EllipsisIcon = Ellipsis;
+
   title = 'paxxing-dashboard';
+
+  protected drawerIsOpen = signal(false);
+
+  protected toggleDrawer() {
+    console.log('hi');
+    this.drawerIsOpen.set(!this.drawerIsOpen());
+  }
 }
