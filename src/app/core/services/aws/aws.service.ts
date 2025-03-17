1648cfdc75c25251aa3c1d073f347e431093bf6c
유태양 <taeyang.yu@autowini.com>
2025-03-17 09:47:39 +0900

    민감한 정보 숨기기

--- src/app/core/services/aws/aws.service.ts
+++ src/app/core/services/aws/aws.service.ts
@@ -10,12 +10,11 @@ export class AwsService {
   private s3Client: S3Client | null = null;
 
   constructor() {
-    // Cognito Identity Pool을 통한 인증 설정
     this.s3Client = new S3Client({
-      region: 'ap-northeast-2', // 이미지에서 확인한 리전
+      region: environment.region,
       credentials: fromCognitoIdentityPool({
-        clientConfig: { region: 'ap-northeast-2' }, // 리전 설정
-        identityPoolId: 'ap-northeast-2:c7dccd4a-a8a0-44ab-9dc7-c6cda32ec634', // 이미지에서 확인한 Identity Pool ID
+        clientConfig: { region: 'ap-northeast-2' },
+        identityPoolId: environment.identityPoolId,
       }),
     });
   }
